b42432b029deab17eee487581d98bad98bd2c7ff
Debora Durek <debora.durek@gmail.com>
2021-07-07 14:08:51 -0500

    refactor: Render question as main quiz component

--- screens/Quiz/Quiz.js
+++ screens/Quiz/Quiz.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import QuizQuestion from './components/QuizQuestion';
-import QuizAnswer from './components/QuizAnswer';
-import QuizScore from './components/QuizScore';
-import { Container, FullWidthContainer } from '../../styles/shared';
+import { View } from 'react-native';
+import ProgressBar from './components/ProgressBar';
+import TextButton from '../../components/TextButton';
+import { Container, FullWidthContainer, LargeCardContainer } from '../../styles/shared';
+import { QAHeaderContainer, QAHeader, QAText, QAButtonContainer } from './styles/styles';
+import { QuizButtonGreen, QuizButtonRed } from '../../components/StyledButtons';
 
 class Quiz extends Component {
   render() {
     return (
       <Container>
         <FullWidthContainer>
-          <QuizQuestion />
-          {/* <QuizAnswer /> */}
-          {/* <QuizScore /> */}
+          <ProgressBar />
+          <LargeCardContainer>
+            <QAHeaderContainer>
+              <QAHeader>Question</QAHeader>
+            </QAHeaderContainer>
+            <View>
+              <QAText>Is "Canada Day" celebrated on the 1st of July?</QAText>
+            </View>
+            <TextButton onPress={() => this.props.navigation.navigate('QuizAnswer')}>
+              Show Answer
+            </TextButton>
+          </LargeCardContainer>
+          <QAButtonContainer>
+            <QuizButtonGreen>V</QuizButtonGreen>
+            <QuizButtonRed>X</QuizButtonRed>
+          </QAButtonContainer>
         </FullWidthContainer>
       </Container>
     );
