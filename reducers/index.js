5f6b8f8a033178db5ac36c9d87bd371a37add373
Debora Durek <debora.durek@gmail.com>
2021-07-07 18:08:38 -0500

    feat: Create initial redux reducers

---
+++ reducers/index.js
@@ -0,0 +1,34 @@
+import { RECEIVE_DECKS, ADD_DECK_TITLE, ADD_CARD_T0_DECK } from '../actions';
+
+export default function decks(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_DECKS:
+      return {
+        ...state,
+        ...action.decks,
+      };
+
+    case ADD_DECK_TITLE:
+      const { title } = action;
+      return {
+        ...state,
+        [title]: {
+          title: [title],
+          questions: [],
+        },
+      };
+
+    case ADD_CARD_T0_DECK:
+      const { title, card } = action;
+      return {
+        ...state,
+        [title]: {
+          ...state[title],
+          questions: [...state[title].questions, card],
+        },
+      };
+
+    default:
+      return state;
+  }
+}
