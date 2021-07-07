2a1c8be1d0fd082df12721534a0249d44498725c
Debora Durek <debora.durek@gmail.com>
2021-07-06 23:27:56 -0500

    feat: Create components for repetitive code

---
+++ screens/Quiz/components/ProgressBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LightText } from '../../../styles/shared';
+import { StyledProgressBar } from '../styles/styles';
+
+function ProgressBar() {
+  return (
+    <StyledProgressBar>
+      <LightText>1 / 7</LightText>
+    </StyledProgressBar>
+  );
+}
+
+export default ProgressBar;
