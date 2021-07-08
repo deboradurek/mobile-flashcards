afbe19b24d560fd70bfa1b1d0259f6e49f760bf1
Debora Durek <debora.durek@gmail.com>
2021-07-07 20:04:03 -0500

    feat: Add state for deck's name input

--- screens/AddDeck/AddDeck.js
+++ screens/AddDeck/AddDeck.js
@@ -9,15 +9,29 @@ import {
 } from '../../styles/shared';
 
 class AddDeck extends Component {
+  state = {
+    deckName: '',
+  };
+
+  handleChange = (e) => {
+    const deckName = e.nativeEvent.text;
+
+    this.setState({
+      deckName,
+    });
+  };
+
   render() {
+    const { deckName } = this.state;
+
     return (
       <Container>
         <FullWidthContainer>
           <Title>What's your new deck's name?</Title>
           <InputContainer>
             <StyledTextInput
-              onChangeText={() => {}}
-              value={() => {}}
+              onChange={this.handleChange}
+              value={deckName}
               placeholder="Enter deck's name"
               maxLength={30}
             />
