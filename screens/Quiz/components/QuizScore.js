585892e8f6a65a1b7682df36510fc1a0287a52df
Debora Durek <debora.durek@gmail.com>
2021-07-13 16:37:05 -0500

    feat: Add dynamic data to score

--- screens/Quiz/components/QuizScore.js
+++ screens/Quiz/components/QuizScore.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { FilledButton } from '../../../components/StyledButtons';
 import Title from '../../../components/Title';
-import { CenterContainer, Container, FullWidthContainer } from '../../../styles/shared';
-import { CardScore, ProgressBar } from '../styles/styles';
+import ProgressBar from './ProgressBar';
+import { FilledButton, OutlinedButton } from '../../../components/StyledButtons';
+import { Container, FullWidthContainer } from '../../../styles/shared';
+import { CardScore, ScoreButtonContainer, TextScore } from '../styles/styles';
 
 class QuizScore extends Component {
   render() {
+    const { numCards, numCorrect } = this.props.route.params;
+    const score = (numCorrect / numCards) * 100;
+
+    const message =
+      numCards !== 1
+        ? `You got ${numCorrect} out of ${numCards} questions!`
+        : `You got ${numCorrect} out of ${numCards} question!`;
+
     return (
       <Container>
         <FullWidthContainer>
           <Title>Your Score</Title>
           <CardScore>
-            <Text>86%</Text>
+            <TextScore>{score}%</TextScore>
           </CardScore>
-          <ProgressBar>
-            <Text>You got 6 out of 7 questions!</Text>
-          </ProgressBar>
-
-          <View>
-            <FilledButton>
-              <Text>Restart</Text>
+          <ProgressBar message={message} />
+          <ScoreButtonContainer>
+            <OutlinedButton onPress={() => this.props.navigation.navigate('Quiz')}>
+              Restart
+            </OutlinedButton>
+            <FilledButton onPress={() => this.props.navigation.navigate('Deck')}>
+              Back to Deck
             </FilledButton>
-            <FilledButton>
-              <Text>Back to Decks</Text>
-            </FilledButton>
-          </View>
+          </ScoreButtonContainer>
         </FullWidthContainer>
       </Container>
     );
