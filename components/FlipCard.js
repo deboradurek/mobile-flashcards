49b6fea52a6b61d02ee3790f08d3850f643c5594
Debora Durek <debora.durek@gmail.com>
2021-07-13 13:22:52 -0500

    refactor: Merge flip card component and animation

--- components/FlipCard.js
+++ components/FlipCard.js
@@ -1,22 +1,105 @@
-import React from 'react';
-import { Animated, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+import { Animated, Easing, StyleSheet, View } from 'react-native';
 import { QAHeader, QAHeaderContainer, QAText } from '../screens/Quiz/styles/styles';
 import TextButton from './TextButton';
 
-function FlipCard({ header, text, buttonText, handlePress, animation }) {
-  return (
-    <Animated.View style={[styles.flipContainer, animation, { width: '100%', height: '100%' }]}>
-      <QAHeaderContainer>
-        <QAHeader>{header}</QAHeader>
-      </QAHeaderContainer>
-      <View>
-        <QAText>{text}</QAText>
-      </View>
-      <TextButton activeOpacity={0} onPress={handlePress}>
-        {buttonText}
-      </TextButton>
-    </Animated.View>
-  );
+class FlipCard extends Component {
+  state = {
+    isAnimatedShowing: false,
+    flipSideOne: new Animated.Value(1),
+    flipSideTwo: new Animated.Value(0),
+  };
+
+  componentDidUpdate(prevProps) {
+    const { flip } = this.props;
+
+    if (flip !== prevProps.flip) {
+      if (flip === true) {
+        this.flipAnimated({ flipOpen: true, flipSideTwoValue: 0, flipSideOneValue: 1 });
+      }
+
+      if (flip === false) {
+        this.flipAnimated({ flipOpen: false, flipSideTwoValue: 1, flipSideOneValue: 0 });
+      }
+    }
+  }
+
+  flipAnimated = ({ flipOpen, flipSideTwoValue, flipSideOneValue }) => {
+    Animated.sequence([
+      Animated.timing(flipOpen ? this.state.flipSideTwo : this.state.flipSideOne, {
+        toValue: flipOpen ? flipSideTwoValue : flipSideOneValue,
+        duration: 150,
+        useNativeDriver: true,
+        easing: Easing.bezier(0.445, 0.05, 0.55, 0.95),
+      }),
+      Animated.timing(flipOpen ? this.state.flipSideOne : this.state.flipSideTwo, {
+        toValue: flipOpen ? flipSideOneValue : flipSideTwoValue,
+        duration: 75,
+        useNativeDriver: true,
+        easing: Easing.bezier(0.445, 0.05, 0.55, 0.95),
+      }),
+    ]).start(() =>
+      this.setState({
+        isAnimatedShowing: false,
+      })
+    );
+  };
+
+  handlePress = () => {
+    const { onFlip } = this.props;
+
+    if (this.state.isAnimatedShowing === true) {
+      return;
+    }
+    this.setState({
+      isAnimatedShowing: true,
+    });
+
+    onFlip();
+  };
+
+  render() {
+    const { frontSide, backSide, flip } = this.props;
+
+    const flipFront = {
+      transform: [
+        {
+          rotateY: this.state.flipSideOne.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['90deg', '0deg'],
+          }),
+        },
+      ],
+    };
+
+    const flipBack = {
+      transform: [
+        {
+          rotateY: this.state.flipSideTwo.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['-90deg', '0deg'],
+          }),
+        },
+      ],
+    };
+
+    const animation = flip ? flipFront : flipBack;
+    const { header, text, buttonText } = flip ? frontSide : backSide;
+
+    return (
+      <Animated.View style={[styles.flipContainer, animation, { width: '100%', height: '100%' }]}>
+        <QAHeaderContainer>
+          <QAHeader>{header}</QAHeader>
+        </QAHeaderContainer>
+        <View>
+          <QAText>{text}</QAText>
+        </View>
+        <TextButton activeOpacity={0} onPress={this.handlePress}>
+          {buttonText}
+        </TextButton>
+      </Animated.View>
+    );
+  }
 }
 
 // Styles for Animated component
--- screens/Quiz/Quiz.js
+++ screens/Quiz/Quiz.js
@@ -11,57 +11,7 @@ class Quiz extends Component {
   state = {
     indexCard: 0,
     numCorrect: 0,
-    isAnimatedShowing: false,
     flip: true,
-    flipSideOne: new Animated.Value(1),
-    flipSideTwo: new Animated.Value(0),
-  };
-
-  shouldComponentUpdate(__, nextState) {
-    if (nextState.flip === this.state.flip) {
-      return true;
-    }
-
-    if (nextState.flip === true) {
-      this.flipAnimated({ flipOpen: true, flipSideTwoValue: 0, flipSideOneValue: 1 });
-    }
-
-    if (nextState.flip === false) {
-      this.flipAnimated({ flipOpen: false, flipSideTwoValue: 1, flipSideOneValue: 0 });
-    }
-
-    return true;
-  }
-
-  handleFlip = () => {
-    if (this.state.isAnimatedShowing === true) {
-      return;
-    }
-    this.setState((currentState) => ({
-      flip: !currentState.flip,
-      isAnimatedShowing: true,
-    }));
-  };
-
-  flipAnimated = ({ flipOpen, flipSideTwoValue, flipSideOneValue }) => {
-    Animated.sequence([
-      Animated.timing(flipOpen ? this.state.flipSideTwo : this.state.flipSideOne, {
-        toValue: flipOpen ? flipSideTwoValue : flipSideOneValue,
-        duration: 150,
-        useNativeDriver: true,
-        easing: Easing.bezier(0.445, 0.05, 0.55, 0.95),
-      }),
-      Animated.timing(flipOpen ? this.state.flipSideOne : this.state.flipSideTwo, {
-        toValue: flipOpen ? flipSideOneValue : flipSideTwoValue,
-        duration: 75,
-        useNativeDriver: true,
-        easing: Easing.bezier(0.445, 0.05, 0.55, 0.95),
-      }),
-    ]).start(() =>
-      this.setState({
-        isAnimatedShowing: false,
-      })
-    );
   };
 
   handleAnswer = (userAnswer) => () => {
@@ -80,6 +30,12 @@ class Quiz extends Component {
     }));
   };
 
+  onFlip = () => {
+    this.setState((currentState) => ({
+      flip: !currentState.flip,
+    }));
+  };
+
   render() {
     const { numCards, deckQuestions } = this.props;
     const { indexCard, flip } = this.state;
@@ -88,46 +44,24 @@ class Quiz extends Component {
     const question = deckQuestions[indexCard].question;
     const answer = deckQuestions[indexCard].answer;
 
-    const flipSideTwo = {
-      transform: [
-        {
-          rotateY: this.state.flipSideOne.interpolate({
-            inputRange: [0, 1],
-            outputRange: ['90deg', '0deg'],
-          }),
-        },
-      ],
-    };
-
-    const flipSideOne = {
-      transform: [
-        {
-          rotateY: this.state.flipSideTwo.interpolate({
-            inputRange: [0, 1],
-            outputRange: ['-90deg', '0deg'],
-          }),
-        },
-      ],
-    };
-
     return (
       <Container>
         <FullWidthContainer>
           <ProgressBar answeredCards={answeredCards} totalCards={numCards} />
           <FullWidthContainer>
             <FlipCard
-              header={'Question'}
-              text={question}
-              buttonText={'Show answer'}
-              handlePress={this.handleFlip}
-              animation={flipSideTwo}
-            />
-            <FlipCard
-              header={'Answer'}
-              text={answer}
-              buttonText={'Back to Question'}
-              handlePress={this.handleFlip}
-              animation={flipSideOne}
+              frontSide={{
+                header: 'Question',
+                text: question,
+                buttonText: 'Show answer',
+              }}
+              backSide={{
+                header: 'Answer',
+                text: answer,
+                buttonText: 'Back to Question',
+              }}
+              onFlip={this.onFlip}
+              flip={flip}
             />
           </FullWidthContainer>
           {flip === true && (
