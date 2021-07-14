b14835bfca5b341d32dc7e1dc0d572283b9840dc
Debora Durek <debora.durek@gmail.com>
2021-07-14 18:20:04 -0500

    style: Created theme using styled components

---
+++ styles/theme.js
@@ -0,0 +1,21 @@
+import { DefaultTheme } from '@react-navigation/native';
+
+export const styledTheme = {
+  darkGray: '#222831',
+  mediumGray: '#393E46',
+  lightGray: '#E8E8E8',
+  pink: '#DA0037',
+  orange: '#F96D00',
+  inactiveTintColorTab: '#6b6b6b',
+  shadowColorWhite: 'rgba(255, 255, 255, 0.34)',
+};
+
+export const darkGray = '#222831';
+
+export const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#393E46',
+  },
+};
--- utils/colors.js
+++
@@ -1,6 +0,0 @@
-export const darkestGray = '#222831';
-export const darkGray = '#393E46';
-export const lightGray = '#E8E8E8';
-export const pink = '#DA0037';
-export const orange = '#F96D00';
-export const mediumGray = '#999';
